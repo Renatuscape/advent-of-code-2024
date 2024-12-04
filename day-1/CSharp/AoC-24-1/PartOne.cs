b5eeb7ecd8c6898f09535284850acdb622242dfd
Renatuscape <silarcta@gmail.com>
2024-12-04 15:30:52 +0100

    Adds completed C# solution for Day 1 Part One

---
+++ day-1/CSharp/AoC-24-1/PartOne.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC_24_1
+{
+    internal partial class Program
+    {
+        internal static class PartOne
+        {
+            internal static void SortLists(List<int> listA, List<int> listB)
+            {
+
+                int distance = 0;
+
+                for (int i = 0; i < listA.Count; i++)
+                {
+                    Console.WriteLine(listA[i] + " - " + listB[i] + " = " + Math.Abs(listA[i] - listB[i]));
+                    distance += Math.Abs(listA[i] - listB[i]);
+                }
+                Console.WriteLine("\nTotal distance: " + distance);
+            }
+        }
+    }
+}
---
+++ day-1/CSharp/AoC-24-1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC_24_1
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input-coa-24-1.txt");
+
+            List<int> listA = new();
+            List<int> listB = new();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var locationIDs = lines[i].Split("   ");
+
+                listA.Add(int.Parse(locationIDs[0]));
+                listB.Add(int.Parse(locationIDs[1]));
+            }
+
+            listA.Sort();
+            listB.Sort();
+
+            // PART ONE
+            PartOne.SortLists(listA, listB);
+        }
+    }
+}
