0ae3267555e123f6bdc511f267a2b9d939f726ef
Renatuscape <silarcta@gmail.com>
2024-12-05 15:56:41 +0100

    Attempts in Kotlin and C# at Day 2 part 2, incomplete. Input files excluded from git

---
+++ day-2/CSharp/AoC-24-2/AoC-24-2/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace AoC_24_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input-coa-24-2.txt");
+            int passes = 0;
+            int reportsChecked = 0;
+
+            foreach (string line in lines)
+            {
+                reportsChecked++;
+                Console.WriteLine($"CHECKING REPORT #{reportsChecked} " + line);
+
+                if (ReportChecker.CheckReport(line, out var problemIndex))
+                {
+                    Console.WriteLine($"\t\tReport passed" + line + "\n");
+                    passes++;
+                }
+                else
+                {
+                    Console.WriteLine($"\t\tReport failed " + line + "\n");
+
+                    var newReport = ReportChecker.ActivateProblemDampener(line, problemIndex);
+
+                    if (ReportChecker.CheckReport(newReport, out var x))
+                    {
+                        Console.WriteLine($"\t\tDampened Report passed " + line + "\n");
+                        passes++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"\n{passes} REPORTS PASSED");
+        }
+    }
+
+    internal static class ReportChecker
+    {
+        public static string ActivateProblemDampener(string report, int problemIndex)
+        {
+            var reportNumbers = report.Split(' ').ToList();
+            reportNumbers.Remove(reportNumbers[problemIndex]);
+
+            report = "";
+
+            foreach (var element in reportNumbers)
+            {
+                report += element + " ";
+            }
+
+            return report.TrimEnd();
+        }
+
+        public static bool CheckReport(string report, out int problemIndex)
+        {
+            var reportNumbers = report.Split(' ');
+            int prevTemp = int.Parse(reportNumbers[0]);
+            int difference = prevTemp - int.Parse(reportNumbers[1]);
+            bool isIncreasing = difference > 0;
+           
+
+            for (int i =  1; i < reportNumbers.Length; i++)
+            {
+                problemIndex = i;
+                var currentTemp = int.Parse(reportNumbers[i]);
+                difference = prevTemp - currentTemp;
+
+                Console.WriteLine($"\t\tThe difference between {prevTemp} and {currentTemp} is {difference}. Temperature is {(isIncreasing ? "increasing" : "decreasing")}");
+
+                if (currentTemp == prevTemp)
+                {
+                    Console.WriteLine($"\t\t!! Current temp equalled previous temp");
+                    return false;
+                }
+                if (Math.Abs(currentTemp - prevTemp) > 3)
+                {
+                    Console.WriteLine($"\t\t!! Temp varied by more than 3");
+                    return false;
+                }
+                if (isIncreasing != difference > 0)
+                {
+                    Console.WriteLine($"\t\t!! Temperature trend changed!");
+                    return false;
+                }
+
+                prevTemp = currentTemp;
+            }
+
+            problemIndex = -1;
+            return true;
+        }
+    }
+}
--- day-2/Kotlin/AoC-24-2/src/Main.kt
+++ day-2/Kotlin/AoC-24-2/src/Main.kt
@@ -1,67 +1,89 @@
 import java.io.File
 import java.io.BufferedReader
+import kotlin.math.abs
+
+var totalSafeReports = 0
 
 fun main() {
     val bufferedReader: BufferedReader = File("src/input-coa-24-2.txt").bufferedReader()
     val inputString = bufferedReader.use { it.readText() }
 
     val reports = inputString.split("\n")
-    var totalSafeReports = 0
     var totalReportsChecked = 0
 
     for (report in reports) {
         totalReportsChecked++
         println("\uD83E\uDD8C #$totalReportsChecked RED-NOSED REPORT " + report)
-
-        if (checkIfReportSafe(report)) {
-            println("\t\uD83C\uDF1F\uD83C\uDF84 Report is SAFE \uD83C\uDF84\uD83C\uDF1F\n")
-            totalSafeReports++
-        } else {
-            println("\t\uD83E\uDD83 Report is NOT SAFE\n")
-        }
+        checkIfReportSafe(report)
     }
 
     println("\nTOTAL SAFE REPORTS: " + totalSafeReports)
 }
 
-fun checkIfReportSafe(report: String): Boolean {
+fun checkIfReportSafe(report: String, activatedProblemDampener: Boolean = false) {
     var isSafe = true
-    val reportNumbers = report.split(" ").map { it.trim() }.filter { it.isNotEmpty() }
-    var prevNumber: Int? = null
+    val reportNumbers = report.split(" ").map { it.trim() }.filter { it.isNotEmpty() }.map { it.toInt() }
     var isIncreasing: Boolean? = null
-    var difference = 0
+    var prevNumber = reportNumbers[0]
 
-    for (i in reportNumbers.indices) {
-        val number = reportNumbers[i].toInt()
+    for (i in 1 until reportNumbers.size) {
+        val currentNumber = reportNumbers[i]
+        val difference = currentNumber - prevNumber
 
-        if (prevNumber == null) {
-            prevNumber = number
-        } else {
-            difference = (number - prevNumber)
+        if (isIncreasing == null) {
+            isIncreasing = difference > 0
+        }
 
-            if (isIncreasing == null){
-                isIncreasing = difference > 0
+        println("\tDifference between $prevNumber and $currentNumber is $difference. Temperature is ${if (isIncreasing) "rising" else "falling"}.")
+
+        if (!checkSingleTemp(currentNumber, prevNumber, difference, isIncreasing)) {
+            if (!activatedProblemDampener) {
+                runProblemDampener(reportNumbers, i)
+                return
             }
-
-            println("\tDifference between $prevNumber and $number is $difference. Temperature is ${if (isIncreasing) "rising" else "falling"}.")
-
-            if (difference == 0) {
-                println("\t❄\uFE0F There was neither an increase nor decrease in temperature!")
+            else{
                 isSafe = false
-                break
-            } else if (difference in -3..3) {
-                if (isIncreasing == difference > 0) {
-                    prevNumber = number
-                } else {
-                    println("\t\uD83E\uDDE3 Report has both increasing and decreasing temperatures!")
-                    isSafe = false
-                    break
-                }
-            } else {
-                isSafe = false
-                break
             }
         }
-        }
-    return isSafe
+
+        prevNumber = currentNumber
+        isIncreasing = difference > 0
+    }
+
+    if (isSafe == true) {
+        println("\t\uD83C\uDF1F\uD83C\uDF84 Report is SAFE \uD83C\uDF84\uD83C\uDF1F\n")
+        totalSafeReports++
+    } else {
+        println("\t\uD83E\uDD83 Report is NOT SAFE\n")
+    }
+}
+
+fun checkSingleTemp(temp: Int, prevTemp: Int, difference: Int, initialTrend: Boolean): Boolean {
+    if (temp == prevTemp) {
+        println("\t❄\uFE0F Temperature cannot be identical!")
+        return false
+    }
+
+    if (abs(difference) > 3) {
+        println("\t\uD83D\uDD34 Temperature change is too volatile!!")
+        return false
+    }
+
+    val currentTrend = difference > 0
+    if (currentTrend != initialTrend) {
+        println("\t\uD83D\uDD25 Temperature trend is inconsistent!")
+        return false
+    }
+
+    return true
+}
+
+fun runProblemDampener(report: List<Int>, problemIndex: Int) {
+    println("\t\uD83E\uDDE3 Activating Problem Dampener for $report")
+    val dampenedReport = report.filterIndexed { index, _ -> index != problemIndex }
+    val dampenedReportString = dampenedReport.joinToString(" ")
+
+    println("\n\t\uD83E\uDD8C Rerunning dampened report: $dampenedReportString")
+
+    return checkIfReportSafe(dampenedReportString, true)
 }
