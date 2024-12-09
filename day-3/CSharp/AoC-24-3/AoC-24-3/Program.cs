4a42689a7750d8a65f0afd6f983cb47156ee15f9
Renatuscape <silarcta@gmail.com>
2024-12-09 13:48:49 +0100

    Solves Day 3 Part 1 in C#

---
+++ day-3/CSharp/AoC-24-3/AoC-24-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC_24_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input-coa-24-3.txt");
+            string pattern = @"mul\(([0-9,]+)\)";
+            int sum = 0;
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                string contents = match.Groups[1].Value;
+                var instructions = contents.Split('\n');
+
+                foreach (var instruction in instructions)
+                {
+                    Console.WriteLine(instruction); // Outputs: data1, data2
+
+                    var numbers = instruction.Split(',');
+                    sum += int.Parse(numbers[0]) * int.Parse(numbers[1]);
+                }
+            }
+
+            Console.WriteLine("\nFINALY SUM: " + sum);
+        }
+    }
+}
