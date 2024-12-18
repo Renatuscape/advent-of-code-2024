d2ae5a73224c7253da0d7aa15e64a5cf0023124b
Renatuscape <silarcta@gmail.com>
2024-12-18 15:33:44 +0100

    Sets up project for Day 12

---
+++ day-12/CSharp/AoC-24-12/AoC-24-12/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AoC_24_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool useExampleInput = false;
+            string input;
+
+            if (useExampleInput)
+            {
+                input = File.ReadAllText("example.txt");
+            }
+            else
+            {
+                input = File.ReadAllText("input.txt");
+            }
+        }
+    }
+}
