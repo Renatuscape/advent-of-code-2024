976cfd5d71de945fb8399345d5800f65b721f4c1
Renatuscape <silarcta@gmail.com>
2024-12-18 11:37:19 +0100

    Dramatically improves performance, allowing the solution to Part 2

--- day-11/CSharp/AoC-24-11/AoC-24-11/Program.cs
+++ day-11/CSharp/AoC-24-11/AoC-24-11/Program.cs
@@ -16,62 +16,90 @@
                 input = File.ReadAllText("input.txt");
             }
 
-            // Use array instead of list for better performance
-            long[] rocks = input.Split(" ")
+            // Parse initial rocks
+            var initialRocks = input.Split(" ")
                 .Select(long.Parse)
-                .ToArray();
+                .ToList();
 
-            Console.WriteLine("Number of rocks: " + rocks.Length);
-            Console.WriteLine(string.Join(" ", rocks) + "\n\n");
-
-            for (int i = 1; i <= blinks; i++)
+            // Use a dictionary to track rock distributions
+            var rockDistribution = new Dictionary<long, long>();
+            foreach (var rock in initialRocks)
             {
-                rocks = Blink(rocks);
-                // Uncomment for verbose output
-                Console.WriteLine("#" + i + " Blink");
+                if (!rockDistribution.ContainsKey(rock))
+                    rockDistribution[rock] = 0;
+                rockDistribution[rock]++;
             }
 
-            Console.WriteLine("Number of rocks: " + rocks.Length);
+            Console.WriteLine("Initial number of rocks: " + initialRocks.Count);
+            PrintDistribution(rockDistribution);
+
+            // Perform blinks
+            for (int i = 1; i <= blinks; i++)
+            {
+                rockDistribution = Blink(rockDistribution);
+                Console.WriteLine($"#Blink {i} - Rocks: {rockDistribution.Sum(x => x.Value)}");
+                // Uncomment to see distribution each blink
+                // PrintDistribution(rockDistribution);
+            }
+
+            Console.WriteLine("Final number of rocks: " + rockDistribution.Sum(x => x.Value));
         }
 
-        public static long[] Blink(long[] rocks)
+        static Dictionary<long, long> Blink(Dictionary<long, long> currentRocks)
         {
-            // Pre-allocate maximum possible size to reduce reallocations
-            var newRocks = new long[rocks.Length * 2];
-            int newRocksCount = 0;
+            var newRocks = new Dictionary<long, long>();
 
-            foreach (var rock in rocks)
+            foreach (var rock in currentRocks)
             {
-                if (rock == 0)
+                long rockValue = rock.Key;
+                long rockCount = rock.Value;
+
+                if (rockValue == 0)
                 {
-                    newRocks[newRocksCount++] = 1;
+                    // 0 becomes 1
+                    AddToDistribution(newRocks, 1, rockCount);
                 }
                 else
                 {
-                    // Use mathematical approach instead of string conversion
-                    long currentRock = rock;
-                    int digitCount = currentRock == 0 ? 1 : (int)Math.Floor(Math.Log10(currentRock) + 1);
-
-                    if (digitCount % 2 == 0)
+                    string rockStr = rockValue.ToString();
+                    if (rockStr.Length % 2 == 0)
                     {
-                        // Split into two parts
-                        long divisor = (long)Math.Pow(10, digitCount / 2);
-                        long leftMark = currentRock / divisor;
-                        long rightMark = currentRock % divisor;
+                        // Split in half
+                        int middleIndex = rockStr.Length / 2;
+                        long leftMark = long.Parse(rockStr.Substring(0, middleIndex));
+                        long rightMark = long.Parse(rockStr.Substring(middleIndex));
 
-                        newRocks[newRocksCount++] = leftMark;
-                        newRocks[newRocksCount++] = rightMark;
+                        AddToDistribution(newRocks, leftMark, rockCount);
+                        AddToDistribution(newRocks, rightMark, rockCount);
                     }
                     else
                     {
                         // Multiply by 2024
-                        newRocks[newRocksCount++] = currentRock * 2024;
+                        AddToDistribution(newRocks, rockValue * 2024, rockCount);
                     }
                 }
             }
 
-            // Create a new array with exactly the number of rocks
-            return newRocks[..newRocksCount];
+            return newRocks;
+        }
+
+        // Helper method to add to rock distribution
+        static void AddToDistribution(Dictionary<long, long> distribution, long rock, long count)
+        {
+            if (!distribution.ContainsKey(rock))
+                distribution[rock] = 0;
+            distribution[rock] += count;
+        }
+
+        // Helper method to print rock distribution
+        static void PrintDistribution(Dictionary<long, long> distribution)
+        {
+            Console.WriteLine("Rock Distribution:");
+            foreach (var rock in distribution.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"Rock {rock.Key}: {rock.Value} times");
+            }
+            Console.WriteLine("Total Rocks: " + distribution.Sum(x => x.Value));
         }
     }
 }
