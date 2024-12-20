aaaa8f3ef38cd9104d15c36ba97483085ed449e8
Renatuscape <silarcta@gmail.com>
2024-12-20 13:56:58 +0100

    Creates separate file for Region class

---
+++ day-12/CSharp/AoC-24-12/AoC-24-12/Region.cs
@@ -0,0 +1,58 @@
+﻿namespace AoC_24_12
+{
+    internal partial class Program
+    {
+        public class Region
+        {
+            public char type;
+            public (int x, int y) startingCoords;
+            public List<Tile> Tiles { get; set; } = new();
+
+            public bool AddTile(Tile tile)
+            {
+                // If this is the first tile, set region type
+                if (Tiles.Count == 0)
+                {
+                    type = tile.type;
+                }
+
+                // Only add if tile matches region type
+                if (tile.type == type)
+                {
+                    Tiles.Add(tile);
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void SetFencing()
+            {
+                foreach (Tile tile in Tiles)
+                {
+                    tile.fencing = (false, false, false, false);
+
+                    // Get all surrounding coordinates for the tile
+                    var adjacentCoordinates = GetAdjacentCoordinates(tile);
+
+                    for (int i = 0; i < adjacentCoordinates.Length; i++)
+                    {
+                        // Check if an adjacent tile exists in the region
+                        if (Tiles.FirstOrDefault(t => t.coords == adjacentCoordinates[i]) == null)
+                        {
+                            //If no adjacent tile exists, turn on fence for the corresponding direction
+                            if (i == 0)
+                            { tile.fencing.N = true; }
+                            else if (i == 1)
+                            { tile.fencing.S = true; }
+                            else if (i == 2)
+                            { tile.fencing.E = true; }
+                            else if (i == 3)
+                            { tile.fencing.W = true; }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
