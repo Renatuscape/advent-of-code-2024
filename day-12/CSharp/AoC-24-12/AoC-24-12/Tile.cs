3c03b3f2865aab114a36c83e66dade1221466f57
Renatuscape <silarcta@gmail.com>
2024-12-20 13:57:06 +0100

    Creates separate file for Tile class

---
+++ day-12/CSharp/AoC-24-12/AoC-24-12/Tile.cs
@@ -0,0 +1,24 @@
+﻿namespace AoC_24_12
+{
+    internal partial class Program
+    {
+        public class Tile
+        {
+            public (int x, int y) coords;
+            public char type;
+            public (bool N, bool S, bool E, bool W) fencing;
+
+            public int GetFencing()
+            {
+                int count = new[] { fencing.N, fencing.S, fencing.E, fencing.W }.Count(b => b);
+                Console.WriteLine($"Number of fenced sides: {count}");
+                return count;
+            }
+
+            public override string ToString()
+            {
+                return type + coords.ToString();
+            }
+        }
+    }
+}
